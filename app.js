9cdaf6fab600c2357fe175173767c75983a9e876
Monroealysia8 <monroealysia8@gmail.com>
2025-02-21 14:07:55 -0600

    commit functions

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 "use strict"
 
+/*www.gemini.com
+will generate jason data for me*/
+
 // const person = {
 // name: "bob",
 // age: 54, 
@@ -67,3 +70,57 @@ const carString = JSON.stringify(car);
 
 
 console.log(carString);
+
+
+
+
+
+/*Async function*/
+async function loadJSONbanana(){
+    const response = await fetch("data.json")
+    const data = await response.json()
+
+    console.log(data);
+
+}
+
+loadJSONbanana();
+
+
+/*Arrow and standard Functions*/
+
+subtractNumber(3, 6, 8);
+function subtractNumber(num1, num2, num3){
+let finalNum = num1 - num2 - num3;
+console.log(this);
+return finalNum;
+}
+
+
+
+const subtract = (num1, num2, num3) => {
+  console.log(this)
+    let finalNum = num1 - num2 - num3;
+  return finalNum;
+};
+
+
+
+
+/*object with method*/
+const person23 = {
+    talk:function(){
+        console.log(this);
+    }
+
+}
+/* and object with method using arrow function*/
+/* be careful using arrow functions and methods....bc it will show window in the console. thats an error. */
+const person24 = {
+  talk: () => {
+    console.log(this);
+  },
+};
+
+
+/*colt stills youtube.video to familiarize yourself with "this"*/
